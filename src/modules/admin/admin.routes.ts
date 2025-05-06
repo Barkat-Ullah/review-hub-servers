6ee807606d07bb48301fed69b780f9b4bbb2a233
MdSaifulIslamRafsan <mdsaifulislamrafsan099@gmail.com>
2025-05-06 19:17:41 +0600

    Local changes before switching to Saiful branch

--- src/modules/admin/admin.routes.ts
+++ src/modules/admin/admin.routes.ts
@@ -4,5 +4,5 @@ import { adminController } from './admin.controller';
 const router = express.Router();
 
 router.get('/', adminController.getDashboardOverview);
-
+router.get('/popular-premium-reviews', adminController.getPopularPremiumReviews);
 export const AdminRoutes = router;
